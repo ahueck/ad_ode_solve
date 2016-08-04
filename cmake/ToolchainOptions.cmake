58655d40773b81e2acd45668c8e0c610a71e3f05
Alexander Hück <hueck.alexander@gmail.com>
2016-08-04 15:58:12 +0200

    Project skeleton

---
+++ cmake/ToolchainOptions.cmake
@@ -0,0 +1,62 @@
+if(NOT DEFINED LOG_LEVEL)
+  set(LOG_LEVEL 1)
+endif()
+add_definitions(-DLOG_LEVEL=${LOG_LEVEL})
+
+# set default build type
+if(NOT CMAKE_BUILD_TYPE)
+  set(CMAKE_BUILD_TYPE Debug)
+endif()
+
+# Misc. find out if clang compiler is used
+if(CMAKE_CXX_COMPILER MATCHES "clang\\+\\+.*")
+  set(CMAKE_COMPILER_IS_CLANGXX 1)
+endif()
+
+# Setup general compiler options
+if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX)
+  set(CXX_ONLY_FLAGS "-std=c++11 -Wall -Wextra -pedantic -Wunreachable-code -Wwrite-strings -Wpointer-arith -Wcast-align -Wcast-qual -Wno-unused-parameter -fno-rtti ${CXX_ONLY_FLAGS}")
+
+  # Release build
+  set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
+  # Release build with debug symbols
+  set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g -O3")
+  # Debug Build
+  set(CMAKE_CXX_FLAGS_DEBUG "-g")
+  # Release build optimized for size
+  set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os")
+endif()
+
+# COMMON_FLAGS is unsused for now
+set(CMAKE_CXX_FLAGS  "${CXX_ONLY_FLAGS} ${COMMON_FLAGS} ${CMAKE_CXX_FLAGS}")
+
+string(TOUPPER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_UPPER)
+
+if(CMAKE_BUILD_TYPE_UPPER STREQUAL "DEBUG")
+  set(EXE_SUFFIX "Debug")
+elseif(CMAKE_BUILD_TYPE_UPPER STREQUAL "RELEASE")
+  set(EXE_SUFFIX "")
+elseif(CMAKE_BUILD_TYPE_UPPER STREQUAL "RELWITHDEBINFO")
+  set(EXE_SUFFIX "RelDebg")
+elseif(CMAKE_BUILD_TYPE_UPPER STREQUAL "MINSIZEREL")
+  set(EXE_SUFFIX "RelMin")
+else()
+  message(WARNING "Unknown exe suffix found.")
+  set(EXE_SUFFIX "Unknown")
+endif()
+
+if(CMAKE_BUILD_TYPE_UPPER STREQUAL "RELWITHDEBINFO")
+  set(ALL_CXX_FLAGS ${CMAKE_CXX_FLAGS_RELWITHDEBINFO})
+elseif(CMAKE_BUILD_TYPE_UPPER STREQUAL "RELEASE")
+  set(ALL_CXX_FLAGS ${CMAKE_CXX_FLAGS_RELEASE})
+elseif(CMAKE_BUILD_TYPE_UPPER STREQUAL "MINSIZEREL")
+  set(ALL_CXX_FLAGS ${CMAKE_CXX_FLAGS_MINSIZEREL})
+else()
+  # Fallback to Debug
+  set(ALL_CXX_FLAGS ${CMAKE_CXX_FLAGS_DEBUG})
+endif()
+
+message(STATUS "Build Type: "             ${CMAKE_BUILD_TYPE})
+message(STATUS "Logging Level: "          ${LOG_LEVEL})
+message(STATUS "Compiler Flags (Modus): " ${ALL_CXX_FLAGS})
+message(STATUS "Compiler Flags (cmake): " ${CMAKE_CXX_FLAGS})
