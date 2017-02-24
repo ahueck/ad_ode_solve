9490df432ccaa3c062148c82ebc3606b175b8a2b
Alexander Hück <hueck.alexander@gmail.com>
2017-02-24 14:52:00 +0100

    Unit test for print function

--- include/Util.h
+++ include/Util.h
@@ -15,6 +15,33 @@
 namespace ode {
 namespace util {
 
+namespace detail {
+
+template <typename T>
+using is_scalar_t = typename std::is_scalar<T>::type;
+
+template <typename T>
+using is_scalar_rem_ref_t = is_scalar_t<std::remove_reference_t<T>>;
+
+template <typename T>
+inline void print(std::true_type, const T& v, std::ostream& out) {
+  out << v;
+}
+
+template <typename Container>
+inline void print(std::false_type, Container&& v, std::ostream& out) {
+  out << "[";
+  auto last = std::end(v) - 1;
+  std::for_each(std::begin(v), last, [&out](const auto& yi) {
+    detail::print(is_scalar_rem_ref_t<decltype(yi)>(), yi, out);
+    out << ", ";
+  });
+  detail::print(is_scalar_rem_ref_t<decltype(*last)>(), *last, out);
+  out << "]";
+}
+
+} /* namespace detail */
+
 template <typename T>
 inline bool less(T a, T b) {
   return b - a > std::numeric_limits<T>::epsilon();
@@ -25,33 +52,10 @@ inline bool less_eq(T a, T b) {
   return a - b <= std::numeric_limits<T>::epsilon();
 }
 
-namespace detail {
-
 template <typename T>
-using is_scalar_t = typename std::is_scalar<T>::type;
-
-template <typename T>
-inline void print(std::true_type, const T& v, std::ostream& out) {
-  out << v;
-}
-
-template <typename Container>
-inline void print(std::false_type, const Container& v, std::ostream& out) {
-  out << "[";
-  auto last = std::end(v) - 1;
-  std::for_each(std::begin(v), last, [&out](const auto& yi) {
-    detail::print(is_scalar_t<std::remove_reference_t<decltype(yi)>>(), yi, out);
-    out << ", ";
-  });
-  detail::print(is_scalar_t<std::remove_reference_t<decltype(*last)>>(), *last, out);
-  out << "]";
-}
-}
-
-template <typename T>
-inline void print(const T& v, std::ostream& out = std::cout) {
-  using detail::is_scalar_t;
-  detail::print(is_scalar_t<T>(), v, out);
+inline void print(T&& v, std::ostream& out = std::cout) {
+  using detail::is_scalar_rem_ref_t;
+  detail::print(is_scalar_rem_ref_t<T>(), std::forward<T>(v), out);
   out << std::endl;
 }
 
--- test/src/test_util.cpp
+++ test/src/test_util.cpp
@@ -32,17 +32,43 @@ TEST_CASE("Test general utility functions", "[utility]") {
   SECTION("Printer") {
     ode::y_series y{{1.5, 2.5}, {3.0, 4.0}};
     ode::t_series t{1.0, 2.3, 3.5, 4.0};
+    ode::t_series t_single{1.7};
+    ode::y_series y_single{{1.5}};
+    std::vector<std::vector<ode::vectory_type>> nested_v{{{1.5, 2.5}, {3.5, 4.5}}, {{5.7, 6.7}}};
+    ode::scalar sc = 0.5;
 
-    std::ostringstream out_t;
-    out_t << std::setprecision(1);
-    out_t << std::fixed;
-    print(t, out_t);
-    REQUIRE("[1.0, 2.3, 3.5, 4.0]\n" == out_t.str());
+    std::ostringstream out;
+    out << std::setprecision(1);
+    out << std::fixed;
+    print(t, out);
+    REQUIRE("[1.0, 2.3, 3.5, 4.0]\n" == out.str());
 
-    std::ostringstream out_y;
-    out_y << std::setprecision(1);
-    out_y << std::fixed;
-    print(y, out_y);
-    REQUIRE("[[1.5, 2.5], [3.0, 4.0]]\n" == out_y.str());
+    out.str("");
+    print(y, out);
+    REQUIRE("[[1.5, 2.5], [3.0, 4.0]]\n" == out.str());
+
+    out.str("");
+    print(nested_v, out);
+    REQUIRE("[[[1.5, 2.5], [3.5, 4.5]], [[5.7, 6.7]]]\n" == out.str());
+
+    out.str("");
+    print(t_single, out);
+    REQUIRE("[1.7]\n" == out.str());
+
+    out.str("");
+    print(y_single, out);
+    REQUIRE("[[1.5]]\n" == out.str());
+
+    out.str("");
+    print(sc, out);
+    REQUIRE("0.5\n" == out.str());
+
+    out.str("");
+    print(0.5, out);
+    REQUIRE("0.5\n" == out.str());
+
+    out.str("");
+    print(std::move(nested_v), out);
+    REQUIRE("[[[1.5, 2.5], [3.5, 4.5]], [[5.7, 6.7]]]\n" == out.str());
   }
 }
