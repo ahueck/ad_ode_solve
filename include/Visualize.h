06dc0bd2cb93b2e8816d07e3bdac402174dbd642
Alexander Hück <hueck.alexander@gmail.com>
2017-03-01 15:42:06 +0100

    Index error

--- include/Visualize.h
+++ include/Visualize.h
@@ -24,12 +24,12 @@ inline void plot(const t_series& t, const y_series& y, std::string plot_file) {
 
   const auto num_y = (*std::begin(y)).size();
 
-  for (size_t i = 1; i <= num_y; ++i) {
+  for (size_t i = 0; i < num_y; ++i) {
     scalar min = std::numeric_limits<scalar>::max();
     scalar max = std::numeric_limits<scalar>::min();
     vectory_type y_i;
     std::stringstream sstream;
-    sstream << "y_" << i;
+    sstream << "y_" << i+1;
     for(const auto& y_t : y) {
       const auto val = y_t[i];
         y_i.push_back(val);
@@ -40,8 +40,8 @@ inline void plot(const t_series& t, const y_series& y, std::string plot_file) {
           max = val;
         }
     }
-    plt::subplot(num_y, 1, i);
-    plt::named_plot(sstream.str(), t, y_i, "o-");
+    plt::subplot(num_y, 1, i+1);
+    plt::plot(t, y_i, {{"linestyle", "-"}, {"marker", "o"}, {"color", "#006DB5" /* #0072BD */}});
     const auto ampl = (max - min) * 0.5 * 0.2;
     plt::ylim(min - ampl, max + ampl);
     plt::ylabel(sstream.str());
